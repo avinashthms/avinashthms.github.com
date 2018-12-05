19e2ecc40fdc43e4fd6e199e5173fdf2ed2bef8e
Avinash <tho16004@byui.edu>
2018-12-04 18:06:36 -0700

    Changes

--- sandbox/minisub/menu.js
+++
@@ -1,9 +0,0 @@
-/* *****************
-* Mmenu activity
-*******************/
-
-// Get menu where clicks will occur
-const MENULINK = document.getElementById("page-nav");
-
-// Interupt the menu link clicks
-Newlink.addEventListener
