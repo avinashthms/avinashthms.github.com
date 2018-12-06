56a5178a45a7cc7fbf3a54ac0176da41cffbeb60
Avinash <tho16004@byui.edu>
2018-12-05 19:17:48 -0700

    x

--- weather-site/js/search.js
+++ weather-site/js/search.js
@@ -20,7 +20,7 @@ searchResults.addEventListener("click", function (event) {
   //   let searchValue = QUERY.value;
   //   // Call the processJSON function to request data and build results
   //   processJSON(searchValue);
-   console.log(event.dataset);
+   console.log(event.target.dataset);
     });
 
 
